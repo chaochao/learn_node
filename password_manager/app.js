7a69195c44cf78fd88d1c495ef04c49f75054655
Chao Liu <liuchao8989@gmail.com>
2016-10-14 13:51:30 -0700

    add function for input from terminal

--- password_manager/app.js
+++ password_manager/app.js
@@ -1,11 +1,71 @@
 //review some javascript and node stuff
+// use 3rd party lib to get data from terminal and store.
 console.log('start passward manager');
 
 var storage = require('node-persist');
-
 storage.initSync();
 
 
+var argv = require('yargs')
+    .command('create',"create account", function (yargs) {
+      yargs.options({
+        name: {
+          demand: true,
+          alias: 'n',
+          description: "name",
+          type: 'string'
+        },
+        username: {
+          demand: true,
+          alias: 'u',
+          description: "username",
+          type: 'string'
+        },
+        passward: {
+          demand: true,
+          alias: 'p',
+          description: "passward",
+          type: 'string'
+        }
+      }).help('help');
+    })
+    .command('get','get account', function (yargs) {
+      yargs.options({
+        name: {
+          demand: true,
+          alias: 'n',
+          description: 'name',
+          type: 'string'
+        }
+      }).help('help')
+    })
+    .help('help')
+    .argv;
+var command = argv._[0]
+// console.log(argv);
+
+
+switch (command) {
+  case 'create':
+    console.log("do create");
+    creatAccount({
+      name: argv.name,
+      username: argv.username,
+      passward: argv.passward});
+    break;
+  case 'get':
+    console.log("do get");
+    var account = getAccount(argv.name);
+    if (typeof account === 'undefined') {
+      console.log("no such account");
+      break;
+    }
+    console.log(account);
+    break;
+    // TODO: edit
+}
+
+// util functions
 function creatAccount(account){
   var accounts = storage.getItemSync('accounts');
 
@@ -14,7 +74,6 @@ function creatAccount(account){
   }
   accounts.push(account);
   storage.setItemSync('accounts',accounts);
-  //this is not good return value
   return account
 }
 
@@ -30,11 +89,9 @@ function getAccount(accountName){
   return matchedAccount;
 }
 
-creatAccount({
-  name: 'test',
-  username: 'some@hotmail.com',
-  password: '123'
-})
 
-var account = getAccount('test');
-console.log(account);
+
+
+
+
+
