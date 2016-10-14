0152f2e4d6c065e15c840c08b7827b161593b640
Chao Liu <liuchao8989@gmail.com>
2016-10-14 16:04:02 -0700

    add encrypt function

--- password_manager/app.js
+++ password_manager/app.js
@@ -1,10 +1,13 @@
 //review some javascript and node stuff
 // use 3rd party lib to get data from terminal and store.
-console.log('start passward manager');
+console.log('start password manager');
 
+//for encrytion. For better design you may want to input from the terminal
+const MASTERCODE = 'abc'
+
+var crypto = require('crypto-js');
 var storage = require('node-persist');
 storage.initSync();
-
 
 var argv = require('yargs')
     .command('create',"create account", function (yargs) {
@@ -21,10 +24,10 @@ var argv = require('yargs')
           description: "username",
           type: 'string'
         },
-        passward: {
+        password: {
           demand: true,
           alias: 'p',
-          description: "passward",
+          description: "password",
           type: 'string'
         }
       }).help('help');
@@ -42,20 +45,16 @@ var argv = require('yargs')
     .help('help')
     .argv;
 var command = argv._[0]
-// console.log(argv);
-
 
 switch (command) {
   case 'create':
-    console.log("do create");
-    creatAccount({
+    saveAccount({
       name: argv.name,
       username: argv.username,
-      passward: argv.passward});
+      password: argv.password});
     break;
   case 'get':
-    console.log("do get");
-    var account = getAccount(argv.name);
+    var account = getDecryptAccount(argv.name);
     if (typeof account === 'undefined') {
       console.log("no such account");
       break;
@@ -66,10 +65,37 @@ switch (command) {
 }
 
 // util functions
-function creatAccount(account){
+
+function getDecryptAccount (accountName) {
+  var account = getAccount(accountName);
+  if (typeof account !== 'undefined') {
+    // Decrypt
+    var bytes  = crypto.AES.decrypt(account.password.toString(), MASTERCODE);
+    var decryptPassword = bytes.toString(crypto.enc.Utf8);
+    account.password = decryptPassword;
+    //it should be decrpted account
+    return account;
+  }
+}
+
+// May have multiple account with same name, no validation for this
+function saveAccount (account) {
+  console.log("encryptPassword");
+  // encrypt return an object, not sure why it's diff from the doc
+  //maybe use the old version?
+  var encryptPassword = crypto.AES.encrypt(account.password, MASTERCODE).toString();
+  var encryptAccount = {
+    name: account.name,
+    username: account.username,
+    password: encryptPassword
+  };
+  creatAccount(encryptAccount);
+}
+
+function creatAccount (account) {
   var accounts = storage.getItemSync('accounts');
 
-  if (typeof accounts === 'undefined') {
+  if (typeof accounts === 'undefined'){
     accounts = [];
   }
   accounts.push(account);
@@ -77,11 +103,11 @@ function creatAccount(account){
   return account
 }
 
-function getAccount(accountName){
+function getAccount (accountName) {
   var accounts = storage.getItemSync('accounts');
   var matchedAccount;
 
-  accounts.forEach( function(account) {
+  accounts.forEach( function (account) {
     if (account.name == accountName) {
       matchedAccount = account;
     }
